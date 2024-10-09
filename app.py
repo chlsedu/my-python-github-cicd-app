40a046f00807e387217c7351a459c300a685215a
chlsedu <cdgedu@qq.com>
2024-10-09 02:57:55 +0000

    modified app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Docker!-8888-9999-0000"
+    return "Hello, Docker!-8888-9999-0000-444444"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
